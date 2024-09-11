08c95464acddeecb3abe5b84c31a744e847adc9e
smsjh2004 <smsjh2004@naver.com>
2024-09-11 17:14:06 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+/*
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// public : 공개범위, static : 사용자 의지대로 실행O, void : 값이 반환 되지 X을 수 도
+
+class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, to World!");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("Hello, World1");
+        System.out.println("Hello, World2");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int a =10;
+        double b = 100;
+        boolean c = true;
+        char d = 'c';
+        string e ='e';
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+    }
+}
+*/
+
+
+
+
+
